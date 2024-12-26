600b3e4735d0e72b021e8c8a3e29bb86fbcd6785
mehran feriduni <m3hr4nus@gmail.com>
2024-12-26 12:03:19 +0330

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { TaskModule } from './task.module';
-import { TcpOptions, Transport } from '@nestjs/microservices';
-
+import { NestFactory } from "@nestjs/core";
+import { TaskModule } from "./task.module";
+import { TcpOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(TaskModule,{transport:Transport.TCP,options:{port:4003,host:'0.0.0.0'}} as TcpOptions);
+  const app = await NestFactory.createMicroservice(TaskModule, {
+    transport: Transport.TCP,
+    options: { port: 4003, host: "0.0.0.0" },
+  } as TcpOptions);
   await app.listen();
-  console.log('task service is runnig in : localhost:4003')
+  console.log("task service is runnig in : localhost:4003");
 }
 bootstrap();
